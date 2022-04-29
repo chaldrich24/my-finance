9f41da4f589d94e1704d29b203273ba80d6f3635
chaldrich24 <chaldrich24@gmail.com>
2022-04-29 17:37:00 -0400

    connect firebase db

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import MonthSummary from './screens/MonthSummary';
+import db from './firebase-config';
+import { collection, getDocs } from 'firebase/firestore';
 
 function App() {
+  const [cats, setCats] = useState();
+  const catCollectionRef = collection(db, 'categories');
+
+  useEffect(() => {
+    const arr = [];
+    const getCats = async () => {
+      const data = await getDocs(catCollectionRef);
+
+      data.forEach((doc) => {
+        arr.push(doc.data());
+      })
+
+      setCats(arr);
+      console.log(arr);
+
+    }
+
+    getCats();
+
+  }, [])
   return (
     <div className="App">
       <MonthSummary />
